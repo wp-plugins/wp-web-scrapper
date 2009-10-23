cbee2894d02c3f1322fba31b20d7c50400648f01
akshay_raje <akshay_raje@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-10-23 20:43:02 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-web-scrapper/tags/1.6@166429 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wpws_index.php
+++ wpws_index.php
@@ -87,15 +87,6 @@ function wpws_curl($url, $agent, $timeout, $return = true, $postargs = '') {
 			curl_setopt($ch, CURLOPT_POST ,1);
 			curl_setopt($ch, CURLOPT_POSTFIELDS,$postargs);
 		}
-		$header	= array( 
-			"Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-			"Accept-Language: en-gb,en;q=0.5",
-			"Accept-Encoding: gzip,deflate",
-			"Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7",
-			"Keep-Alive: 300",
-			"Connection: keep-alive"
-		);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $header);	
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_USERAGENT, $agent);
 		curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -106,12 +97,6 @@ function wpws_curl($url, $agent, $timeout, $return = true, $postargs = '') {
 		curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
 		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
 		$html = curl_exec($ch);
-		$content_type = curl_getinfo( $ch, CURLINFO_CONTENT_TYPE );
-		preg_match( '@([\w/+]+)(;\s+charset=(\S+))?@i', $content_type, $matches );
-		if ( isset( $matches[1] ) )
-			$mime = $matches[1];
-		if ( isset( $matches[3] ) )
-			$charset = $matches[3];
 		if (empty($html)) {
 			$curl[0] = false;
 			$curl[1] = curl_error($ch);
